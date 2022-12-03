2cd038bdbfd612b773bb32d5406b684c7995d44c
geonya <geony8410@gmail.com>
2022-12-03 17:21:50 +0900

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CustomerComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,58 @@
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatTable } from '@angular/material/table';
+import { Customer } from './customer';
+
+@Component({
+  selector: 'customer',
+  styleUrls: ['customer.component.scss'],
+  templateUrl: 'customer.component.html',
+})
+export class CustomerComponent {
+  customers: Customer[] = [];
+  isAddNew: boolean = false;
+  firstName = new FormControl('', [
+    Validators.required,
+    Validators.minLength(1),
+  ]);
+  lastName = new FormControl('', [
+    Validators.required,
+    Validators.minLength(1),
+  ]);
+  displayedColumns: string[] = ['firstName', 'lastName'];
+
+  @ViewChild(MatTable) table: MatTable<Customer>;
+
+  constructor() {
+    this.customers.push(new Customer('geony', 'han'));
+    this.customers.push(new Customer('bora', 'lee'));
+    this.customers.push(new Customer('solhee', 'han'));
+  }
+
+  toggle() {
+    this.isAddNew = !this.isAddNew;
+  }
+  reset() {
+    this.firstName.setValue('');
+    this.lastName.setValue('');
+  }
+  onSubmit() {
+    if (!this.firstName.value || !this.lastName.value) return;
+    this.customers.push(
+      new Customer(this.firstName.value, this.lastName.value),
+    );
+    this.toggle();
+    this.table.renderRows();
+  }
+
+  getFirstNameErrorMsg() {
+    if (this.firstName.hasError('required')) return '내용을 입력해주세요.';
+    if (this.firstName.hasError('minlength')) return '2자 이상 입력해주세요';
+    return '';
+  }
+  getLastNameErrorMsg() {
+    if (this.lastName.hasError('required')) return '내용을 입력해주세요.';
+    if (this.lastName.hasError('minlength')) return '2자 이상 입력해주세요';
+    return '';
+  }
+}
